400dfd1b19c1f10a3338957a6bf83e44264df41f
somedude314 <asif.mahbub@outlook.com>
2018-07-06 01:36:54 -0400

    changed

--- src/main/java/edu/concordia/PasswordMemorizationApplication.java
+++ src/main/java/edu/concordia/PasswordMemorizationApplication.java
@@ -20,9 +20,9 @@ public class PasswordMemorizationApplication implements CommandLineRunner{
 	@Override
 	public void run(String... strings) throws Exception {
 
-		repository.save(new Guess("abc","cde",1,"abbbbb","bbbc","abbbbb",1));
-		repository.save(new Guess("abc","aad",1,"abbbbb","asdf","abbbbb",3));
-		repository.save(new Guess("abc","uva",1,"abbbbb","dfasdf","abbbbb",3));
+		//repository.save(new Guess("abc","cde",1,"abbbbb","bbbc","abbbbb",1));
+		//repository.save(new Guess("abc","aad",1,"abbbbb","asdf","abbbbb",3));
+		//repository.save(new Guess("abc","uva",1,"abbbbb","dfasdf","abbbbb",3));
 		//System.out.println("guess record :" +repository.getOne(1));
 	}
 
