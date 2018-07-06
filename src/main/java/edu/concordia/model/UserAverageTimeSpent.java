87bb0287b05a9d360e522792a2701cfc2814dc23
somedude314 <asif.mahbub@outlook.com>
2018-07-05 22:34:03 -0400

    changed

--- src/main/java/edu/concordia/model/UserAverageTimeSpent.java
+++ src/main/java/edu/concordia/model/UserAverageTimeSpent.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 /**
- * Created by Abdullah Al Amin on 7/6/2018.
+ * Created by Asif Mahbub on 7/6/2018.
  */
 @AllArgsConstructor
 public class UserAverageTimeSpent {
